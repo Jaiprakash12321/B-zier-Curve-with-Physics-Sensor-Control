7154d6ee8cbf50b06bb4242788754be5db028b4a
Your Name <your.email@example.com>
2025-12-14 08:18:43 +0530

    Initial Commit

---
+++ ContentView.swift/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  ContentView.swift
+//
+//  Created by Jaiprakash Yadav on 14/12/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ ContentView.swift/ContentView_swiftApp.swift
@@ -0,0 +1,17 @@
+//
+//  ContentView_swiftApp.swift
+//  ContentView.swift
+//
+//  Created by Jaiprakash Yadav on 14/12/25.
+//
+
+import SwiftUI
+
+@main
+struct ContentView_swiftApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
